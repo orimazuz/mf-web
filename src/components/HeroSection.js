951060ce202727d2ae586f320fefedcfdd7c9c84
Ori Mazuz <orimazuz@macbook-pro.home>
2020-11-07 13:37:44 +0200

    Responsive title font size

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,31 +4,27 @@ import Video from './Video';
 import { Link } from 'react-router-dom';
 
 class HeroSection extends React.Component {
-  renderHome() {
+  renderHero() {
     if (this.props.heroMode) {
-      return this.renderHero();
+        return (
+        <>
+            <h2>Debut Album Out Now!</h2>
+            <Link to='/listen' className='album-link' onClick={() => {this.changeSection("LISTEN", false)}}>
+                <img className='album-artwork' src='/images/mind-art.jpg' alt='Artwork'/>
+            </Link>
+        </>
+        );
     }
     return;
-  }
-
-  renderHero() {
-    return (
-      <>
-        <h2>Debut Album Out Now!</h2>
-          <Link to='/listen' className='album-link' onClick={() => {this.changeSection("LISTEN", false)}}>
-              <img className='album-artwork' src='/images/mind-art.jpg' alt='Artwork'/>
-          </Link>
-      </>
-    );
   }
 
   render () {
       return (
         <div className={this.props.heroMode ? 'hero-container' : 'title-container'}>
             <Video src='/videos/mf-title-720.mp4' thumb='/images/video-thumb.png' autoPlay="autoplay" loop="loop" muted="muted" />
-            <div className="titel">
+            <div className="title">
                 <h1>{this.props.title}</h1>
-                {this.renderHome()}        
+                {this.renderHero()}        
             </div>
         </div>
       );
